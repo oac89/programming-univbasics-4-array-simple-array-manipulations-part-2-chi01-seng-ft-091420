6ddb41fd7615b08350a4c2d3e9f04a531dd13deb
Oscar A Castro <oacastro2@gmail.com>
2020-08-26 22:35:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,11 +19,11 @@ def using_flatten(array)
 end 
 
 def using_delete(array, string)
-  array.delete = string 
+  array.delete= string 
 
 end 
 
 def using_delete_at(array, integer)
-  array.delete_at[integer]
+  array.delete_at(integer)
   
 end   
